b1b19eeaf9cc466a023bdc08642511daefdc6c56
rpruiz33 <70825109+rpruiz33@users.noreply.github.com>
2022-06-10 19:35:40 -0300

    componente item

--- task-list333/src/app/components/tasks/tasks.component.ts
+++ task-list333/src/app/components/tasks/tasks.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Task } from '../../Task';
 import { TASKS } from '../../mock-Tasks';
+
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
@@ -8,6 +9,7 @@ import { TASKS } from '../../mock-Tasks';
 })
 export class TasksComponent implements OnInit {
   tasks:Task  []=TASKS  ;
+ 
   constructor() { }
 
   ngOnInit(): void {
